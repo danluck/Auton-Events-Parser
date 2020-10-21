c9e7402de3d45e6501e4c4eefe8fd8f97dcde5f6
Mikhail Isaev <danluck@mail.ru>
2020-10-21 21:51:20 +0300

    Add console windown, add get byte length window

---
+++ AutonEventsParser/AutonEventsParser/Deserializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutonEventsParser
+{
+    class Deserializer
+    {
+        public UInt32 GetByteArrayFromHexText(String input)
+        {
+            UInt32 length = (uint)(input.Length / 2);
+            return length;
+        }
+
+        public byte[] StringToByteArray(String hex)
+        {
+            int NumberChars = hex.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
+        }
+    }
+}
